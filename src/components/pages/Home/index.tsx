d51cc41b785bc55d4f47b8a074c96005b8894cef
igor rebeche <igorrebeche@gmail.com>
2020-05-04 00:32:52 -0300

    feat: start mouting home page

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,15 +1,37 @@
 import React from "react";
 import Card from "../../Card";
-import { Wrapper } from "./styles";
+import {
+  Wrapper,
+  Button,
+  CollectedWrapper,
+  FinanceNumber,
+  Section
+} from "./styles";
+import Header from "../../Header";
 
 interface Props {}
 
 export default function Home(props: Props) {
   return (
     <Wrapper>
-      <Card>
-        <p> AAAAAa </p>
-      </Card>
+      <Header>
+        <Section>
+          <CollectedWrapper>
+            <span>Arrecadado</span>
+            <div>
+              <FinanceNumber size={`medium`}>{"R$ 20000,00"}</FinanceNumber>
+              <FinanceNumber size={`small`}>{"/ R$ 1.000.000"}</FinanceNumber>
+            </div>
+          </CollectedWrapper>
+        </Section>
+        <Section>
+          <Button> Compre Pixels! </Button>
+        </Section>
+        <Section>
+          <p style={{ alignSelf: `left` }}> Como Funciona? </p>
+        </Section>
+      </Header>
+      <Card height={'500px'} width={'1500px'}>aaaa</Card>
     </Wrapper>
   );
 }
--- src/components/pages/Home/styles.tsx
+++ src/components/pages/Home/styles.tsx
@@ -1,8 +1,41 @@
 import styled from "styled-components";
-import Theme from "../../;
 
 export const Wrapper = styled.div`
   display: flex;
-  padding: 3rem;
-  background: ${props => props.theme.}
+  padding: 1.4rem;
+  flex-direction: column;
 `;
+
+
+export const Button = styled.button`
+  border-radius: 10px;
+  background-color: transparent;
+  border-color: ${props => props.theme.colors.secondary};
+  border-style: solid;
+  color: ${props => props.theme.colors.white};
+`
+
+export const CollectedWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-self: right;
+  p:first-child {
+    font-size: ${props => props.theme.fontSizes.small}
+  }
+`
+
+interface financeProps {
+  size?: "medium" | "large" | "small"
+}
+
+export const FinanceNumber = styled.span<financeProps>`
+  value : ${props => props.size};
+  padding: 4px;
+  font-size: ${props => props.size ? props.theme.fontSizes[props.size] : `` };
+`
+
+export const Section = styled.div`
+    width: 300px;
+    display: flex;
+    justify-content: center;
+`
